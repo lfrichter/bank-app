04ef579c024040311e618d038983da6594bdef1f
Luis Fernando Richter <fernando.richter@gmail.com>
2022-12-29 12:04:55 -0300

    build(Vite config): Adjustes to deploy production

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
+    build: {
+        manifest: true,
+    },
+    resolve: {
+        alias: [
+            {
+                find: '@/',
+                replacement: path.resolve(__dirname, './resources/ts')
+            }
+        ]
+    },
     plugins: [
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    // includeAbsolute: false,
+                },
+            },
+        }),
         laravel([
             'resources/ts/app.ts',
         ]),
